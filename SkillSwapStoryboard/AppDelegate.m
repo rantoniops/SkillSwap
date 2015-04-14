dc550939ebea1f087974f766baef20d0a84d4f01
Tommy Gibbons <tommygibbons46@gmail.com>
2015-04-13 18:39:16 -0700

    map loads

--- SkillSwapStoryboard/AppDelegate.m
+++ SkillSwapStoryboard/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <Parse/Parse.h>
 
 @interface AppDelegate ()
 
@@ -16,7 +17,18 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    // [Optional] Power your app with Local Datastore. For more info, go to
+    // https://parse.com/docs/ios_guide#localdatastore/iOS
+    [Parse enableLocalDatastore];
+
+    // Initialize Parse.
+    [Parse setApplicationId:@"qhaDPNU0OQAYXjTIm0eALSxIxxsfSDnctf89Rx2W"
+                  clientKey:@"OgQkJHVBqW2D3CdmHhNyqJ7CSr3oXYlvekgJ6WI3"];
+
+    // [Optional] Track statistics around application opens.
+    [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
+
+    // ...
     return YES;
 }
 
---
+++ SkillSwapStoryboard/MapVC.m
@@ -0,0 +1,59 @@
+//
+//  ViewController.m
+//  SkillSwapStoryboard
+//
+//  Created by Antonio Perez on 4/13/15.
+//  Copyright (c) 2015 antonioperez. All rights reserved.
+//
+
+#import "MapVC.h"
+#import <CoreLocation/CoreLocation.h>
+#import <MapKit/MapKit.h>
+
+@interface MapVC () <MKMapViewDelegate, CLLocationManagerDelegate>
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property CLLocationManager *locationManager;
+
+@end
+
+@implementation MapVC
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.mapView.showsUserLocation = true;
+    self.locationManager = [CLLocationManager new];
+    [self.locationManager requestWhenInUseAuthorization];
+    self.locationManager.delegate = self;
+}
+
+
+
+-(void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation
+{
+    CLLocationCoordinate2D location;
+    location.latitude = userLocation.coordinate.latitude;
+    location.longitude = userLocation.coordinate.longitude;
+    MKCoordinateSpan span = MKCoordinateSpanMake(0.1,0.1);
+    [self.mapView setRegion:MKCoordinateRegionMake(location,span) animated:true];
+
+}
+- (IBAction)onAddButtonTap:(UIButton *)sender
+{
+    MKPointAnnotation *newAnnotation = [[MKPointAnnotation alloc]init];
+    [self.mapView addAnnotation:newAnnotation];
+}
+
+- (void)mapView:(MKMapView *)mapView annotationView:(MKAnnotationView *)annotationView didChangeDragState:(MKAnnotationViewDragState)newState fromOldState:(MKAnnotationViewDragState)oldState
+{
+    if (newState == MKAnnotationViewDragStateStarting)
+    {
+        annotationView.dragState = MKAnnotationViewDragStateDragging;
+    }
+    else if (newState == MKAnnotationViewDragStateEnding || newState == MKAnnotationViewDragStateCanceling)
+    {
+        annotationView.dragState = MKAnnotationViewDragStateNone;
+    }
+}
+
+@end
