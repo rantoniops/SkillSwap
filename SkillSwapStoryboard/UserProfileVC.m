018a00e683f28f9887490a706717b5147734b254
Tommy Gibbons <tommygibbons46@gmail.com>
2015-04-16 12:53:04 -0700

    can save a photo to userProfileNow

--- SkillSwapStoryboard/UserProfileVC.m
+++ SkillSwapStoryboard/UserProfileVC.m
@@ -8,28 +8,108 @@
 @property (weak, nonatomic) IBOutlet UILabel *skills;
 @property (weak, nonatomic) IBOutlet UITableView *tableVIew;
 @property (weak, nonatomic) IBOutlet UILabel *descriptionText;
+
+
+@property UIImage *chosenImage;
+@property NSData *smallImageData;
+
+
 @end
 @implementation UserProfileVC
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    [self queryForUserInfo];
+    
+    self.profileImage.userInteractionEnabled = YES;
+    UIImage *profileImage = [UIImage imageNamed:@"emptyProfile"];
+    self.profileImage.image = profileImage;
+    
+    self.profileImage.frame = CGRectMake(0, 0, 250, 250);
+    self.profileImage.layer.masksToBounds = YES;
+    self.profileImage.layer.borderWidth = 1;
+    UITapGestureRecognizer *photoTap = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(handleTap:)];
+    [self.profileImage addGestureRecognizer:photoTap];
+}
+
+
+-(void)queryForUserInfo
+{
     User *currentUser = [User currentUser];
     PFQuery *queryForCourses = [User query];
     [queryForCourses whereKey:@"courses" equalTo:currentUser];
     [queryForCourses findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error)
-    {
-        if (error == nil) {
-            NSLog(@"%@", objects);
-        }
+     {
+         if (error == nil) {
+             NSLog(@"here are the course %@", objects);
+             NSLog(@"%@", currentUser);
+         }
+     }];
+    self.name.text = currentUser.username;
+    //    self.skills.text = currentUser.skills;
+    
+    
+}
+
+
+-(void)handleTap:(UITapGestureRecognizer *)tapGestureRecognizer
+{
+    NSLog(@"successful Tap");
+    [self showAlertOnViewController];
+}
+
+
+-(void)showAlertOnViewController
+{
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Choose photo option" message:nil preferredStyle:UIAlertControllerStyleActionSheet];
+    UIAlertAction *takePhoto = [UIAlertAction actionWithTitle:@"Take a Photo" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        [self showTakePhotoView];
+    }];
+    UIAlertAction *pullLibrary = [UIAlertAction actionWithTitle:@"Choose From Library" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        [self showCameraPhotoView];
     }];
     
-    self.name.text = currentUser.username;
-//    self.skills.text = currentUser.skills;
-
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
+        nil;
+    }];
     
-
-
+    [alert addAction:takePhoto];
+    [alert addAction:pullLibrary];
+    
+    [alert addAction:cancelAction];
+    [self presentViewController:alert animated:true completion:nil];
 }
+
+-(void)showTakePhotoView
+{
+    UIImagePickerController *picker = [[UIImagePickerController alloc]init];
+    picker.delegate = self;
+    picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+    [self presentViewController:picker animated:true completion:NULL];
+    
+}
+
+-(void)showCameraPhotoView
+{
+    UIImagePickerController *picker = [[UIImagePickerController alloc]init];
+    picker.delegate = self;
+    picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    picker.mediaTypes = [UIImagePickerController availableMediaTypesForSourceType:UIImagePickerControllerSourceTypePhotoLibrary];
+    [self presentViewController:picker animated:true completion:NULL];
+}
+
+
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    self.chosenImage = info[UIImagePickerControllerOriginalImage];
+    self.profileImage.image = self.chosenImage;
+    self.smallImageData = UIImageJPEGRepresentation(self.chosenImage, 0.5);
+    [picker dismissViewControllerAnimated:YES completion:NULL];
+    [self saveImage];
+}
+
+
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
@@ -45,6 +125,28 @@
 
 
 
-
+- (IBAction)onSaveButtonPressed:(UIButton *)sender
+{
+    
+}
+-(void)saveImage
+{
+    User *userToSave = [User currentUser];
+    PFFile *imageFile = [PFFile fileWithData:self.smallImageData];
+    userToSave.profilePic = imageFile;
+    [userToSave saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error)
+     {
+         if (succeeded)
+         {
+             NSLog(@"profile pic saved");
+         }
+         else
+         {
+             NSLog(@"profile pic NOT saved");
+         }
+     }];
+    
+    
+}
 
 @end
