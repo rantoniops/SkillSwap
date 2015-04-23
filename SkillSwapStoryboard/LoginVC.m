1593d182e7c7a31e7b8fb2d8056919633aac87c0
Antonio <rantoniops@gmail.com>
2015-04-23 15:32:12 -0700

    list fix

--- SkillSwapStoryboard/LoginVC.m
+++ SkillSwapStoryboard/LoginVC.m
@@ -16,6 +16,9 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    PFObject *testObject = [PFObject objectWithClassName:@"TestObject"];
+    testObject[@"foo"] = @"bar";
+    [testObject saveInBackground];
     self.passwordTextField.secureTextEntry = YES;
 }
 
