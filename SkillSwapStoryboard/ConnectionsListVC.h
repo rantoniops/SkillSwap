ca49ea7ee653e27fbba303fd5020ee7803fbe551
Antonio <rantoniops@gmail.com>
2015-04-26 12:40:06 -0700

    button outlet

--- SkillSwapStoryboard/ConnectionsListVC.h
+++ SkillSwapStoryboard/ConnectionsListVC.h
@@ -1,11 +1,3 @@
-//
-//  ConnectionsListVC.h
-//  SkillSwapStoryboard
-//
-//  Created by Antonio Perez on 4/26/15.
-//  Copyright (c) 2015 antonioperez. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface ConnectionsListVC : UIViewController
