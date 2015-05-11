76eb5b0941e4be500f2dcaca0b6bba74e9378e5d
Antonio <rantoniops@gmail.com>
2015-05-10 19:26:22 -0700

    added new classes VC

---
+++ ClassesListVC.m
@@ -0,0 +1,20 @@
+#import "ClassesListVC.h"
+@interface ClassesListVC ()
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segmendtedControl;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@end
+@implementation ClassesListVC
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+
+
+- (IBAction)segmentedControlPressed:(UISegmentedControl *)sender
+{
+}
+
+
+
+@end
